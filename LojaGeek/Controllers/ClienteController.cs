554ec50e5c710c5be94f53bfd8ef2b213c78e3b2
daniloitagyba <daniloitagyba@gmail.com>
2018-03-16 16:52:15 -0300

    Login e cadastro de cliente

---
+++ LojaGeek/Controllers/ClienteController.cs
@@ -0,0 +1,50 @@
+﻿using LojaGeek.Model.DB;
+using LojaGeek.Model.DB.Model;
+using LojaGeek.Model.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaGeek.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult CadastrarCliente()
+        {
+            return View(new Cliente());
+        }
+
+        public ActionResult EntrarCliente()
+        {
+            return View();
+        }
+
+        public ActionResult Logar(string email, string senha)
+        {
+            LoginUtils.Logar(email, senha);
+
+            if (LoginUtils.Cliente != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("EntrarCliente");
+            }
+        }
+
+        public ActionResult DeslogarCliente()
+        {
+            LoginUtils.Deslogar();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult GravarCliente(Cliente cliente)
+        {
+            DbFactory.Instance.ClienteRepository.SaveOrUpdate(cliente);
+            return RedirectToAction("EntrarCliente");
+        }
+    }
+}
